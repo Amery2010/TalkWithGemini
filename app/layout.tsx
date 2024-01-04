32ff194049d2e2528aa14f5b3b10881b4d55a179
Amery2010 <amery@xiangfa.org>
2024-01-05 05:08:06 +0800

    feat: Supports injecting scripts through global variables, such as Google Analytics, sentry and other scripts feat: Project supports static page export

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import ThemeProvider from '@/components/ThemeProvider'
 import StoreProvider from '@/components/StoreProvider'
 import I18Provider from '@/components/I18nProvider'
@@ -7,8 +8,10 @@ import 'katex/dist/katex.min.css'
 import 'highlight.js/styles/a11y-light.css'
 import './globals.css'
 
+const HEAD_SCRIPTS = process.env.HEAD_SCRIPTS as string
+
 export const metadata: Metadata = {
-  title: 'Talk With Gemini - 与 Gemini 交谈',
+  title: 'Talk with Gemini - 与 Gemini 交谈',
   description: 'Talk with Gemini via voice. 通过声音与 Gemini 交谈。',
   keywords: ['Gemini', 'Gemini Pro', 'Gemini Chat', 'AI', 'voice', 'Free Chatgpt', 'Chatgpt'],
 }
@@ -16,6 +19,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>{HEAD_SCRIPTS ? <Script id="headscript">{HEAD_SCRIPTS}</Script> : null}</head>
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <StoreProvider>
--- next.config.js
+++ next.config.js
@@ -2,5 +2,10 @@
 const nextConfig = {
   transpilePackages: ['crypto-js'],
 }
+if (process.env.NEXT_BUILD_EXPORT) {
+  nextConfig.output = 'export'
+  // Only used for static deployment, the default deployment directory is the root directory
+  nextConfig.basePath = ''
+}
 
 module.exports = nextConfig
